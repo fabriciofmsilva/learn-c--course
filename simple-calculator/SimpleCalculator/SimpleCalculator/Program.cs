5e11c3058141dfe82eae655ce10e21d20b278ca1
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-19 18:54:24 -0200

    variables - simple calculator

--- simple-calculator/SimpleCalculator/SimpleCalculator/Program.cs
+++ simple-calculator/SimpleCalculator/SimpleCalculator/Program.cs
@@ -6,7 +6,19 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string someText = "Here is some text";
+            int number = 10;
+            double anotherNumber = 10.9;
+            bool isOrdered = true;
+            string input = Console.ReadLine();
+            var anything = 10.0;
+
+            Console.WriteLine(someText);
+            Console.WriteLine(number);
+            Console.WriteLine(anotherNumber);
+            Console.WriteLine(isOrdered);
+            Console.WriteLine(input);
+            Console.WriteLine(anything);
         }
     }
 }
