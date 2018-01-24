697e52b0afa2acdb503ad00db2baba7726953841
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-24 13:00:19 -0200

    String Manipulation, Formatting and More

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,8 +6,45 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            int result = 20 / (5 * 2);
-            Console.WriteLine(result);
+            // comparison
+            string someText = "Here is some text";
+            string someOtherText = "Here is some other text";
+
+            someText.Equals(someOtherText);
+            bool isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
+
+            someOtherText = "Here is some text";
+
+            isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
+
+            someText = "Here is Some text";
+            isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
+
+            isEqual = someText.Equals(someOtherText, StringComparison.OrdinalIgnoreCase);
+
+            // concatenation
+            string addedText = someText + " " + someOtherText + " here is some more text.";
+            string formattedString = string.Format("{0} {1} here is some more text.", someText, someOtherText);
+
+            Console.WriteLine(addedText);
+            Console.WriteLine(formattedString);
+
+            // lengths and indexes
+            Console.WriteLine(someText.Length);
+            Console.WriteLine(someText[8]);
+
+            // substring
+            Console.WriteLine(someText.Substring(8, 4));
+
+            // uppercase and lowercase
+            Console.WriteLine(someText.ToLower());
+            Console.WriteLine(someText.ToUpper());
+
+            string anotherText = string.Empty; // string anotherText = "";
+
+            string replacedText = someText.Replace("text", "string");
+
+            Console.WriteLine(replacedText);
         }
     }
 }
