331c4ec238106bb9229323fd6ef49a373182dd64
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-24 12:41:47 -0200

    Converting String To Number

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace SimpleCalculator
+﻿using System;
+
+namespace SimpleCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+
+            int convertedInputToNumber;
+            int.TryParse(input, out convertedInputToNumber);
+
             int number = 90;
-            int result = 10 + 10 - 100 + 1000 - number;
+            int result = 10 + 10 - 100 + 1000 - number + convertedInputToNumber;
         }
     }
 }
