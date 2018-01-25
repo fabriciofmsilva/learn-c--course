a1b9720431f5723ddf111231bea2a4323e22b1d7
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-25 12:35:53 -0200

    Conditional Statements

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,45 +6,34 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            // comparison
-            string someText = "Here is some text";
-            string someOtherText = "Here is some other text";
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
 
-            someText.Equals(someOtherText);
-            bool isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
+            if (username.Equals("Avetis") && password.Equals("abc123"))
+            {
+                Console.WriteLine("Hello Admin!");
+            }
+            else if (username.Equals("Joe") && password.Equals("abc1234"))
+            {
+                Console.WriteLine("Hello Moderator!");
+            }
+            else
+            {
+                Console.WriteLine("Hello Guest!");
+            }
 
-            someOtherText = "Here is some text";
-
-            isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
-
-            someText = "Here is Some text";
-            isEqual = someText.Equals(someOtherText, StringComparison.Ordinal);
-
-            isEqual = someText.Equals(someOtherText, StringComparison.OrdinalIgnoreCase);
-
-            // concatenation
-            string addedText = someText + " " + someOtherText + " here is some more text.";
-            string formattedString = string.Format("{0} {1} here is some more text.", someText, someOtherText);
-
-            Console.WriteLine(addedText);
-            Console.WriteLine(formattedString);
-
-            // lengths and indexes
-            Console.WriteLine(someText.Length);
-            Console.WriteLine(someText[8]);
-
-            // substring
-            Console.WriteLine(someText.Substring(8, 4));
-
-            // uppercase and lowercase
-            Console.WriteLine(someText.ToLower());
-            Console.WriteLine(someText.ToUpper());
-
-            string anotherText = string.Empty; // string anotherText = "";
-
-            string replacedText = someText.Replace("text", "string");
-
-            Console.WriteLine(replacedText);
+            switch (username)
+            {
+                case "Avetis":
+                    Console.WriteLine("Hello Admin!");
+                    break;
+                case "Joe":
+                    Console.WriteLine("Hello Moderator!");
+                    break;
+                default:
+                    Console.WriteLine("Hello Guest!");
+                    break;
+            }
         }
     }
 }
