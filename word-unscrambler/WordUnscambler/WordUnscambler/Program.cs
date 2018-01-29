203e7afcc4813b542f142cbe46f07711c115983e
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-29 18:58:48 -0200

    Lists

---
+++ word-unscrambler/WordUnscambler/WordUnscambler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordUnscambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVarFirstList = new List<int>();
+            List<int> myFirstList = new List<int>();
+
+            Console.WriteLine(myFirstList.Count);
+
+            myFirstList.Add(10);
+            myFirstList.Add(5);
+            //myFirstList.Add("text");
+
+            Console.WriteLine(myFirstList.Count);
+        }
+    }
+}
