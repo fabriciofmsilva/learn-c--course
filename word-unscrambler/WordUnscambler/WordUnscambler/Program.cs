c33b561cc20258be72280f9a51e59d72600966a3
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-30 12:34:58 -0200

    Iterating Lists and Arrays

--- word-unscrambler/WordUnscambler/WordUnscambler/Program.cs
+++ word-unscrambler/WordUnscambler/WordUnscambler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WordUnscambler
 {
@@ -7,10 +8,35 @@ namespace WordUnscambler
         static void Main(string[] args)
         {
             int[] array = { 1, 5, 6, 9, 10 };
+            List<int> list = new List<int>() { 1, 5, 6, 9, 10 };
 
             foreach (var element in array)
             {
                 Console.WriteLine(element);
+            }
+
+            // not make sanse for this case, foreach is more clean
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            // not make sense for this case
+            int index = 0;
+            while (index < array.Length)
+            {
+                Console.WriteLine(array[index]);
+                index++;
+            }
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
             }
         }
     }
