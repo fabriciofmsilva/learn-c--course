40c199349d930c6ab13380227e6b7d77f922e636
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-01-31 12:57:46 -0200

    DRY Principle

--- word-unscrambler/WordUnscambler/WordUnscambler/Program.cs
+++ word-unscrambler/WordUnscambler/WordUnscambler/Program.cs
@@ -7,15 +7,23 @@ namespace WordUnscambler
     {
         static void Main(string[] args)
         {
-            string[] lines = { "This is the first line", "This is the second line", "This is the third line" };
-            File.WriteAllLines("MyFirstFile.txt", lines);
+            string[] lines = { "First line", "Second line", "Third line" };
+            File.WriteAllLines("FormattedFile.txt", FormattedLines(lines));
 
-            string[] fileContents = File.ReadAllLines("MyFirstFile.txt");
+            string[] otherLines = { "Another first line", "Another second line", "Another third line" };
+            File.WriteAllLines("AnotherFormattedFile.txt", FormattedLines(otherLines));
+        }
 
-            foreach(string line in File.ReadLines("MyFirstFile.txt"))
+        static string[] FormattedLines(String[] unformattedLines)
+        {
+            string[] formattedLines = new string[unformattedLines.Length];
+
+            for (int i = 0; i < formattedLines.Length; i++)
             {
-                Console.WriteLine(line);
+                formattedLines[i] = String.Format("{0} {1} {2}", "---", unformattedLines[i], "---");
             }
+
+            return formattedLines;
         }
     }
 }
