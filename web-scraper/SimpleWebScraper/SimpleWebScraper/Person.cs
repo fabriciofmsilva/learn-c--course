7e1e409e1935c336653bd5af32cd4b07698ae5f8
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-05 18:55:35 -0200

    Properties

--- web-scraper/SimpleWebScraper/SimpleWebScraper/Person.cs
+++ web-scraper/SimpleWebScraper/SimpleWebScraper/Person.cs
@@ -1,9 +1,54 @@
-﻿namespace SimpleWebScraper
+﻿using System;
+
+namespace SimpleWebScraper
 {
     class Person
     {
-        private string _ssn;
-        private string _passportData;
-        private string _drivesLicenceNumber;
+        private string _ssn = "SSN";
+        private string _passportData = "Data";
+        private string _drivesLicenceNumber = "More Data";
+        private string _firstName = "Avetis";
+        private string _lastName = "Ghukasyan";
+
+        public bool HasProperDocuments {
+            get
+            {
+                return _ssn.Length > 0 && _passportData.Length > 0 && _drivesLicenceNumber.Length > 0;
+            }
+        }
+
+        public string FirstName {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (value.Length < 1)
+                {
+                    Console.WriteLine("Input is not accepted.");
+                    return;
+                }
+
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (value.Length < 1)
+                {
+                    Console.WriteLine("Input is not accepted.");
+                    return;
+                }
+
+                _lastName = value;
+            }
+        }
     }
 }
--- web-scraper/SimpleWebScraper/SimpleWebScraper/Program.cs
+++ web-scraper/SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -7,6 +7,16 @@ namespace SimpleWebScraper
         static void Main(string[] args)
         {
             Person person = new Person();
+            person.FirstName = "Aventis";
+            person.LastName = "Ghukasyan";
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+
+            Console.WriteLine(person.HasProperDocuments);
+
+            person.FirstName = "";
+            Console.WriteLine(person.FirstName);
         }
     }
 }
