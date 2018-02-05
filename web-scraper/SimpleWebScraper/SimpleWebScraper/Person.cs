11bac187b2fa4f29554cf1359e32b53cc128da0c
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-05 18:44:17 -0200

    Fields

---
+++ web-scraper/SimpleWebScraper/SimpleWebScraper/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleWebScraper
+{
+    class Person
+    {
+        private string _ssn;
+        private string _passportData;
+        private string _drivesLicenceNumber;
+    }
+}
---
+++ web-scraper/SimpleWebScraper/SimpleWebScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+        }
+    }
+}
