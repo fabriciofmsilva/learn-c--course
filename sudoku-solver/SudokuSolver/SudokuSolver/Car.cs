a408073e3e9fb886528a48d6e3a8a976b874a221
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-27 12:56:24 -0300

    Dependency Injection

---
+++ sudoku-solver/SudokuSolver/SudokuSolver/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Car
+    {
+        private bool _on;
+
+        public void TurnOnOff()
+        {
+            _on = !_on;
+            Console.WriteLine(_on ? "The car is on!" : "The car is off!");
+        }
+
+        public void Drive()
+        {
+            if (_on)
+            {
+                Console.WriteLine("Drive car");
+            }
+            else
+            {
+                Console.WriteLine("have to start car first!");
+            }
+        }
+    }
+}
---
+++ sudoku-solver/SudokuSolver/SudokuSolver/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace SudokuSolver
+{
+    class Person
+    {
+        private Car _car;
+
+        public Person(Car car)
+        {
+            _car = car;
+        }
+
+        public void Drive()
+        {
+            _car.TurnOnOff();
+            _car.Drive();
+        }
+    }
+}
