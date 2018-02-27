e37b8f431ac4f73506e4024e897a2083951d3057
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-27 12:46:17 -0300

    StringBuilder

---
+++ sudoku-solver/SudokuSolver/SudokuSolver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder myString1 = new StringBuilder();
+            myString1.Append("Here is some text");
+            myString1.Append("Here is some more text");
+            myString1.Append("Here is even more text");
+
+            string myString2 = "Here is some text";
+            myString2 = myString2 + "Here is some more text";
+            myString2 = myString2 + "Here is even more text";
+        }
+    }
+}
