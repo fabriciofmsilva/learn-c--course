429809708562739b8fcdc3915ae84aa058dd562d
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-28 12:14:48 -0300

    LINQ Introduction

--- sudoku-solver/SudokuSolver/SudokuSolver/Program.cs
+++ sudoku-solver/SudokuSolver/SudokuSolver/Program.cs
@@ -1,16 +1,32 @@
-﻿namespace SudokuSolver
+﻿using System.Linq;
+
+namespace SudokuSolver
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Car ferrari = new Ferrari();
-            Person person = new Person(ferrari);
-            person.Drive();
+            Hobbie[] hobbies = { new Hobbie(1, "Walking"), new Hobbie(2, "Swimming"), new Hobbie(3, "Bodybuilding") };
 
-            Car lamborghini = new Lamborghini();
-            Person person2 = new Person(lamborghini);
-            person.Drive();
+            var hoobie = hobbies.Where(h => h.Name.Equals("Walking"));
+            var hoobieName = hobbies.Where(h => h.Name.Equals("Walking")).Select(h => h.Name);
+            var anotherHobbie = from h in hobbies where h.Name.Equals("Walking") select h.Name ;
+            var allHoobiesWithIdGreaterThenOne = hobbies.Where(h => h.Id > 1);
+
+            var everyHobbieExceptFirstOne = hobbies.Skip(1);
+            var swimmingHobbie = hobbies.Skip(1).Take(1);
         }
+    }
+
+    class Hobbie
+    {
+        public Hobbie(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
