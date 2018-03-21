4d0e78f04adc2741ec5d382e2f39af4cbfc34a68
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-03-21 19:04:58 -0300

    Fixing Few Bugs

--- sudoku-solver/SudokuSolver/SudokuSolver/Strategies/NakedPairsStrategy.cs
+++ sudoku-solver/SudokuSolver/SudokuSolver/Strategies/NakedPairsStrategy.cs
@@ -72,7 +72,7 @@ namespace SudokuSolver.Strategies
                 return;
             }
 
-            for (int row = 0; row < sudokuBoard.GetLength(1); row++)
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
             {
                 if (sudokuBoard[row, givenCol] != sudokuBoard[givenRow, givenCol] && sudokuBoard[row, givenCol].ToString().Length > 1)
                 {
@@ -83,7 +83,7 @@ namespace SudokuSolver.Strategies
 
         private bool HasNakedPairInCol(int[,] sudokuBoard, int givenRow, int givenCol)
         {
-            for (int row = 0; row < sudokuBoard.GetLength(1); row++)
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
             {
                 if (givenRow != row && IsNakedPair(sudokuBoard[row, givenCol], sudokuBoard[givenRow, givenCol]))
                 {
@@ -102,9 +102,9 @@ namespace SudokuSolver.Strategies
 
             var sudokuMap = _sudokuMapper.Find(givenRow, givenCol);
 
-            for (int row = sudokuMap.StartRow; row < sudokuMap.StartRow + 2; row++)
+            for (int row = sudokuMap.StartRow; row <= sudokuMap.StartRow + 2; row++)
             {
-                for (int col = sudokuMap.StartCol; col < sudokuMap.StartCol + 2; col++)
+                for (int col = sudokuMap.StartCol; col <= sudokuMap.StartCol + 2; col++)
                 {
                     if (sudokuBoard[row, col].ToString().Length > 1 && sudokuBoard[row, col] != sudokuBoard[givenRow, givenCol])
                     {
@@ -124,7 +124,7 @@ namespace SudokuSolver.Strategies
                     var elementInSameBlock = _sudokuMapper.Find(givenRow, givenCol).StartRow == _sudokuMapper.Find(row, col).StartRow &&
                         _sudokuMapper.Find(givenRow, givenCol).StartCol == _sudokuMapper.Find(row, col).StartCol;
 
-                    if (!elementSame && elementInSameBlock && !IsNakedPair(sudokuBoard[givenRow, givenCol], sudokuBoard[row, col])
+                    if (!elementSame && elementInSameBlock && IsNakedPair(sudokuBoard[givenRow, givenCol], sudokuBoard[row, col])
                     {
                         return true;
                     }
