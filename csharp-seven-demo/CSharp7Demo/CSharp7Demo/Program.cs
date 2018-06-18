3feb87c405306c5da81b7ac0326b31090ae6f04f
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-06-18 19:11:41 -0300

    Expression Bodies

--- csharp-seven-demo/CSharp7Demo/CSharp7Demo/Program.cs
+++ csharp-seven-demo/CSharp7Demo/CSharp7Demo/Program.cs
@@ -4,6 +4,12 @@ namespace CSharp7Demo
 {
     class Program
     {
+        private int _score = 100;
+        public int MyScore {
+            get => _score;
+            set => _score = value > 0 ? value : throw new Exception("Cannot have a score of 0 or below");
+        }
+
         static void Main(string[] args)
         {
             byte binaryNumber = 0b111;
--- simple-calculator/SimpleCalculator/InputConverter.cs
+++ simple-calculator/SimpleCalculator/InputConverter.cs
@@ -4,11 +4,9 @@ namespace SimpleCalculator
 {
     public class InputConverter
     {
-        public double ConvertInputToNumeric(string argTextInput)
-        {
-            return double.TryParse(argTextInput, out double convertedNumber)
-                ? convertedNumber
+        public double ConvertInputToNumeric(string argTextInput) => 
+            double.TryParse(argTextInput, out double convertedNumber) 
+                ? convertedNumber 
                 : throw new ArgumentException("Expected a numeric value.");
-        }
     }
 }
