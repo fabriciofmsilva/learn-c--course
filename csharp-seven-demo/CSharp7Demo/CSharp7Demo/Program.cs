b3167074e094071854e9f8250f564464452ef0ee
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-06-18 19:05:01 -0300

    Binary Literals & Digit Separators

---
+++ csharp-seven-demo/CSharp7Demo/CSharp7Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp7Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte binaryNumber = 0b111;
+            Console.WriteLine(Convert.ToInt16(binaryNumber));
+
+            long veryLongNumber = 100_000_000_000;
+            Console.WriteLine(veryLongNumber);
+        }
+    }
+}
