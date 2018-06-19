985edac780f7b085dd5b88bb9ddf8b55cf9cbd8e
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-06-19 00:14:17 -0300

    Ref Locals & Returns

--- csharp-seven-demo/CSharp7Demo/CSharp7Demo/Program.cs
+++ csharp-seven-demo/CSharp7Demo/CSharp7Demo/Program.cs
@@ -4,19 +4,13 @@ namespace CSharp7Demo
 {
     class Program
     {
-        private int _score = 100;
-        public int MyScore {
-            get => _score;
-            set => _score = value > 0 ? value : throw new Exception("Cannot have a score of 0 or below");
-        }
-
         static void Main(string[] args)
         {
-            byte binaryNumber = 0b111;
-            Console.WriteLine(Convert.ToInt16(binaryNumber));
+            BinaryLiteralsAndDigitSeparators binaryLiteralsAndDigitSeparators = new BinaryLiteralsAndDigitSeparators();
+            binaryLiteralsAndDigitSeparators.Run();
 
-            long veryLongNumber = 100_000_000_000;
-            Console.WriteLine(veryLongNumber);
+            RefLocalsAndReturns refLocalsAndReturns = new RefLocalsAndReturns();
+            refLocalsAndReturns.Run();
         }
     }
 }
